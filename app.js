516e3a8f2f9c91d0fa96413aaf3e1d6ec72d7596
Than Htut Zaw <zgyi025@gmail.com>
2021-12-25 08:12:57 +0630

    ui update

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 //selectors
-
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 //event listeners
+todoButton.addEventListener('click',addTodo);
 
 //functions
+function addTodo(event) {
+    
+    event.preventDefault();
+
+    //div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add('todo');
+
+    //li 
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //check button
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML='<i class="fas fa-check"></i>';
+    completeButton.classList.add('complete-btn');
+    todoDiv.appendChild(completeButton);
+
+    //trash button 
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+
+    todoInput.value = "";
+}
