422fe5277f50bdc86d63fc18a8909af97f1b5274
Than Htut Zaw <zgyi025@gmail.com>
2021-12-28 15:49:49 +0630

    filter done

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const classbtn = document.getElementsByClassName('todo-button::after');
 // const button = document.querySelector('.todo-button::after');
+const todoSelect = document.querySelector('.filter-todo');
 
 //event listeners
-todoButton.addEventListener('click',addTodo);
-todoList.addEventListener('click',deleteCheck);
-
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+todoSelect.addEventListener('click', filterTodo);
 
 // todoButton.addEventListener('click',btn);
 //functions
 function addTodo(event) {
-    
+
     event.preventDefault();
 
     //div
@@ -22,47 +23,88 @@ function addTodo(event) {
 
     //li 
     const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+    const inputValue = todoInput.value;
+    if (inputValue.trim() == "") {
+        alert('Write something');
+    } else {
 
-    //check button
-    const completeButton = document.createElement('button');
-    completeButton.innerHTML='<i class="fas fa-check"></i>';
-    completeButton.classList.add('complete-btn');
-    todoDiv.appendChild(completeButton);
+        newTodo.innerText = todoInput.value;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
 
-    //trash button 
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML='<i class="fas fa-trash"></i>';
-    trashButton.classList.add('trash-btn');
-    todoDiv.appendChild(trashButton);
+        //check button
+        const completeButton = document.createElement('button');
+        completeButton.innerHTML = '<i class="fas fa-check"></i>';
+        completeButton.classList.add('complete-btn');
+        todoDiv.appendChild(completeButton);
 
-    todoList.appendChild(todoDiv);
+        //trash button 
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add('trash-btn');
+        todoDiv.appendChild(trashButton);
 
-    todoInput.value = "";
+        todoList.appendChild(todoDiv);
+
+        todoInput.value = "";
+    }
 }
 
 // function btn(){
 //     classbtn.stye.setProperty("opacity","0");
-    
+
 // }
 
-function deleteCheck(e){
+function deleteCheck(e) {
     const item = e.target;
-    if (item.classList[0] === 'trash-btn'){
+    if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
         todo.classList.add('fall');
-        todo.addEventListener('transitionend',function(){
-            todo.remove(); 
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
         })
         // todo.remove();
     }
-    if (item.classList[0] === 'complete-btn'){
+    if (item.classList[0] === 'complete-btn') {
         const todo = item.parentElement;
         todo.firstChild.classList.toggle('completed'); //fast child is important and idk why
         // todoList.classList.toggle('completed');
 
 
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach((todo) => {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+
+            case 'completed':
+                if (todo.firstChild.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+
+                }
+                else {
+                    todo.style.display = 'none';
+                }
+                break;
+
+            case 'uncompleted':
+                if (!todo.firstChild.classList.contains('completed')) {
+
+                    todo.style.display = 'flex';
+
+                }
+                else {
+
+                    todo.style.display = 'none';
+                }
+                break;
+
+        }
+    })
+
 }
